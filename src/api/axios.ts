182e58f1d3fea51187da058414480872ff3b8693
Jorge Andrés  Gomez Diaz <JORGEANDRESG1207@GMAIL.COM>
2025-12-14 15:18:27 -0500

    Change API base URL to production endpoint

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: 'https://jluxuries-api.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
